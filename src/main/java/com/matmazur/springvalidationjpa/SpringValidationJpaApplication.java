1920413110f7dab5e395796303b0d319e5832f44
Bingo Bobo <matmazur90@gmail.com>
2019-01-10 20:00:33 +0100

    implemented adding feature in post method

--- src/main/java/com/matmazur/springvalidationjpa/SpringValidationJpaApplication.java
+++ src/main/java/com/matmazur/springvalidationjpa/SpringValidationJpaApplication.java
@@ -1,24 +1,13 @@
 package com.matmazur.springvalidationjpa;
 
-import com.matmazur.springvalidationjpa.model.Person;
-import com.matmazur.springvalidationjpa.services.PersonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringValidationJpaApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(SpringValidationJpaApplication.class, args);
-        PersonService personService = ctx.getBean(PersonService.class);
-
-        Person person = new Person("Jack", "Black", "nice@emailbutstillwrong", 52);
-        Person person2 = new Person("Jack", "", null, -52);
-
-        personService.add(person);
-        personService.add(person2);
-
+        SpringApplication.run(SpringValidationJpaApplication.class, args);
     }
 }
 
