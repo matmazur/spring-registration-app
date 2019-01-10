5088842b8b0f64e87bf53072b487e90501ebee7d
Bingo Bobo <matmazur90@gmail.com>
2019-01-10 16:24:28 +0100

    added repository for Person class

---
+++ src/main/java/com/matmazur/springvalidationjpa/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.matmazur.springvalidationjpa.repositories;
+
+import com.matmazur.springvalidationjpa.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person,Long> {
+}
