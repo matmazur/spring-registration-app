1f564114884abedbab009a37133f2babc80e2520
Bingo Bobo <matmazur90@gmail.com>
2019-01-10 15:30:23 +0100

    initial commit

---
+++ src/main/java/com/matmazur/springvalidationjpa/SpringValidationJpaApplication.java
@@ -0,0 +1,14 @@
+package com.matmazur.springvalidationjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidationJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValidationJpaApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/matmazur/springvalidationjpa/SpringValidationJpaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.matmazur.springvalidationjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringValidationJpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
