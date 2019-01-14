38a1055b933493e267d2b7c6802e96515a68133a
Bingo Bobo <matmazur90@gmail.com>
2019-01-14 14:59:34 +0100

    number of small fixes + added clear list option and clear by id option under the list

--- src/main/java/com/matmazur/springvalidationjpa/controllers/IndexController.java
+++ src/main/java/com/matmazur/springvalidationjpa/controllers/IndexController.java
@@ -1,15 +1,34 @@
 package com.matmazur.springvalidationjpa.controllers;
 
+import com.matmazur.springvalidationjpa.model.Person;
+import com.matmazur.springvalidationjpa.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Comparator;
+import java.util.List;
+
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(ModelMap modelMap) {
-        return "index";
+    private final
+    PersonRepository personRepository;
+
+    @Autowired
+    public IndexController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
     }
 
+    @GetMapping("/")
+    public String index(ModelMap modelMap) {
+
+        List<Person> people = personRepository.findAll();
+        if (!people.isEmpty()) {
+            people.sort(Comparator.comparing(Person::getId));
+            modelMap.put("people", people);
+        }
+        return "index";
+    }
 }
--- src/main/java/com/matmazur/springvalidationjpa/controllers/PersonController.java
+++ src/main/java/com/matmazur/springvalidationjpa/controllers/PersonController.java
@@ -1,13 +1,14 @@
 package com.matmazur.springvalidationjpa.controllers;
 
 import com.matmazur.springvalidationjpa.model.Person;
+import com.matmazur.springvalidationjpa.repositories.PersonRepository;
+import com.matmazur.springvalidationjpa.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -15,11 +16,18 @@ import java.util.List;
 @Controller
 public class PersonController {
 
+    private final
+    PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @GetMapping("/add-person")
     public String index(ModelMap modelMap) {
 
         modelMap.put("person", new Person());
-
         return "add-person";
     }
 
@@ -27,14 +35,31 @@ public class PersonController {
     public String index(@Valid @ModelAttribute Person person, BindingResult result) {
         if (result.hasErrors()) {
             List<ObjectError> errors = result.getAllErrors();
-
-            errors.forEach(x ->
-                    System.out.println(x.getDefaultMessage()));
+            errors.forEach(x -> System.out.println(x.getDefaultMessage()));
         }
 
         if (result.hasErrors()) {
             return "add-person";
         }
-        return "index";
+
+        personRepository.save(person);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/clear-list")
+    public String clearList() {
+        personRepository.deleteAll();
+        return "redirect:/";
+    }
+
+    @PostMapping("/clear-id")
+    public String clearId(@RequestParam(name = "id") Long id, ModelMap modelMap) {
+
+        System.out.println(modelMap.get("id"));
+        if (id != null) {
+            personRepository.deleteById(id);
+        }
+        return "redirect:/";
     }
 }
